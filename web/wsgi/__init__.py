cdc29818e852d0fac3b6d6c86cb649a9d18de3c2
Matthew Tadd <matt.tadd@gmail.com>
2012-04-06 16:26:44 -0400

    Fix bug in wsgi script

--- web/wsgi/__init__.py
+++ web/wsgi/__init__.py
@@ -3,6 +3,7 @@ sys.path.insert(0,"../..")
 import flask
 from trials import TRIALS, pick_players
 from mapgen import AI,CLASS,SOCIETY,MAPSIZE
+import copy
 app = flask.Flask(__name__)
 
 @app.route('/')
@@ -12,7 +13,7 @@ def show_trial_list():
 
 @app.route('/trial/<int:num>')
 def show_trial(num):
-   trial = TRIALS[num-1]
+   trial = copy.deepcopy(TRIALS[num-1])
    players = pick_players(trial['classes'],trial['levels'],
                    trial.get('teams',None),trial.get('sets',None))
    return flask.render_template('trial.html', trial=trial, players=players,
